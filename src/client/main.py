02e91d92e0a4935b26a0b4128918ebd97f96fb3a
fairay <ffairay@gmail.com>
2023-12-04 23:22:14 +0300

    switched server and client

--- src/client/main.py
+++ src/client/main.py
@@ -1,54 +1,105 @@
-import socket
+import asyncio
 import pickle
 import struct
+import time
 
-from vidgear.gears import VideoGear
+import cv2
 
-FRAME_BATCH_SIZE = 16
-INPUT_FILE = "/Users/ivavse/temp/nets/Poopy-di-Scoop.mp4"
+HOST = ""
+PORT = 8089
+
+CHUNK_SIZE = 4096
 
 
-def main():
-    stream = VideoGear(source=INPUT_FILE).start()
+class ServerFetcher:
+    def __init__(self) -> None:
+        self.frame_buffer = asyncio.Queue()
+        self.data = bytes()
 
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect(("localhost", 8089))
+    async def start_server(self):
+        self.server = await asyncio.start_server(self.handle_conn, HOST, PORT)
+        await self.server.serve_forever()
+        print("Socket created")
 
-    def get_raw_frame() -> bytes:
-        frame = stream.read()
-        # Serialize frame
-        data = pickle.dumps(frame)
-        return data
+    async def handle_conn(self, reader, writer):
+        print("Socket bind complete")
+        print("Socket now listening")
+        self.reader = reader
+        await self.run()
 
-    while True:
-        raw_frames = []
-        for frame_number in range(FRAME_BATCH_SIZE):
-            data = get_raw_frame()
-            if len(data) == 4:
-                # end of frames
+    async def recv(self, n: int):
+        return await self.reader.read(n)
+
+    async def recv_data(self, size: int):
+        while len(self.data) < size:
+            self.data += await self.recv(CHUNK_SIZE)
+        msg = self.data[:size]
+        self.data = self.data[size:]
+        return msg
+
+    def sync_run(self):
+        asyncio.run(self.run())
+
+    async def fetch_frames(self):
+        packed_frame_count = await self.recv_data(struct.calcsize("L"))
+        frame_count = struct.unpack("L", packed_frame_count)[0]
+
+        frames = []
+        for frame_i in range(frame_count):
+            packed_frame_meta = await self.recv_data(struct.calcsize("LL"))
+            frame_number, frame_size = struct.unpack("LL", packed_frame_meta)
+            frame_data = await self.recv_data(frame_size)
+            # Extract frame
+            frame = pickle.loads(frame_data)
+            frames.append(frame)
+        return frames
+
+    async def run(self):
+        for i in range(10000):
+            frames = await self.fetch_frames()
+            if len(frames) == 0:
+                print("recived end of communication message")
+
+            print(f"[{i}] recived {len(frames)} frames")
+            for frame in frames:
+                await self.frame_buffer.put(frame)
+
+        self.server.close()
+
+
+def run_fetcher(fetcher: ServerFetcher):
+    async def _run():
+        await fetcher.start_server()
+
+    asyncio.run(_run())
+
+
+async def main():
+    fetcher = ServerFetcher()
+    task = asyncio.create_task(asyncio.to_thread(run_fetcher, fetcher))
+
+    print("start display loop")
+    t = time.time()
+    for i in range(10000):
+        frame = None
+        for timeout_retry in range(5):
+            try:
+                frame = await asyncio.wait_for(fetcher.frame_buffer.get(), timeout=1)
                 break
-            frame_number_raw = struct.pack("L", frame_number)
-            frame_size_raw = struct.pack("L", len(data))
-            raw_frame = frame_number_raw + frame_size_raw + data
-            raw_frames.append(raw_frame)
+            except TimeoutError:
+                print("timeout")
 
-        if len(raw_frames) == 0:
+        if frame is None:
             break
 
-        # Send message length first
-        frame_count_raw = struct.pack("L", len(raw_frames))
-        packed_frames = struct.pack(''.join([str(len(x)) + 's' for x in raw_frames]), *raw_frames)
-        raw_message = frame_count_raw + packed_frames
-        # Then data
-        clientsocket.sendall(raw_message)
+        print(f"[{i}] displaying frame, delay={time.time() - t}")
+        t = time.time()
+        cv2.imshow("frame", frame)
+        cv2.waitKey(1)
 
-        if len(raw_frames) != FRAME_BATCH_SIZE:
-            break
-
-    raw_message = struct.pack("L", 0)
-    clientsocket.sendall(raw_message)
-    print("sended end of communication message")
+    # cv2.imshow("frame", None)
+    task.cancel()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
